029e158c655297e82137ebb2abf738bc678feb63
SantiagoSzpakowski <szsantiago97@gmail.com>
2024-08-20 13:04:17 -0300

    Registro cursos

--- AppTerceraEntrega/urls.py
+++ AppTerceraEntrega/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('inicio/', views.inicio, name='inicio'),
-    path('alumnos/', views.alumnos, name='alumnos')
+    path('alumnos/', views.alumnos, name='alumnos'),
+    path('cursos/', views.cursos, name='cursos')
 ]
--- AppTerceraEntrega/views.py
+++ AppTerceraEntrega/views.py
@@ -7,13 +7,18 @@ from AppTerceraEntrega.models import Alumno, Curso, Profesor
 def inicio(req):
     return render(req, 'appterceraentrega/padre.html')
 
-def alumnos(req):
-    
+def alumnos(req):    
     if req.method == 'POST':
-        print(req.POST)
         alumno = Alumno(nombre=req.POST['nombre'], apellido=req.POST['apellido'], mail=req.POST['mail'])
-        
         alumno.save()
         return render(req,"appterceraentrega/padre.html")
 
     return render(req,"appterceraentrega/alumnos.html")
+
+def cursos(req):
+    if req.method == 'POST':
+        curso = Curso(nombre=req.POST['nombre'], camada=req.POST['camada'])
+        curso.save()
+        return render(req,"appterceraentrega/padre.html")
+
+    return render(req,"appterceraentrega/cursos.html")
