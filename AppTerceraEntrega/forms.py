633601d8deb9831f94fbc54d9f38bf7b22d0399a
SantiagoSzpakowski <szsantiago97@gmail.com>
2024-08-20 15:39:55 -0300

    Registro profesores y cursos con django

--- AppTerceraEntrega/forms.py
+++ AppTerceraEntrega/forms.py
@@ -3,4 +3,13 @@ from django import forms
 class AlumnoFormulario(forms.Form):
     nombre= forms.CharField(max_length=40)
     apellido= forms.CharField(max_length=40)
-    mail= forms.CharField(max_length=50)
+    mail= forms.EmailField(max_length=50)
+
+class ProfesorFormulario(forms.Form):
+    nombre= forms.CharField(max_length=40)
+    apellido= forms.CharField(max_length=40)
+    profesion= forms.CharField(max_length=50)
+
+class CursoFormulario(forms.Form):
+    nombre= forms.CharField(max_length=40)
+    camada= forms.IntegerField()
--- AppTerceraEntrega/views.py
+++ AppTerceraEntrega/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from AppTerceraEntrega.models import Alumno, Curso, Profesor
-from AppTerceraEntrega.forms import AlumnoFormulario
+from AppTerceraEntrega.forms import AlumnoFormulario, ProfesorFormulario, CursoFormulario
 
 # Create your views here.
 
 def inicio(req):
     return render(req, 'appterceraentrega/padre.html')
-
-def profesores(req):    
-    if req.method == 'POST':
-        alumno = Profesor(nombre=req.POST['nombre'], apellido=req.POST['apellido'], profesion=req.POST['profesion'])
-        alumno.save()
-        return render(req,"appterceraentrega/padre.html")
-
-    return render(req,"appterceraentrega/profesores.html")
-
-def cursos(req):
-    if req.method == 'POST':
-        curso = Curso(nombre=req.POST['nombre'], camada=req.POST['camada'])
-        curso.save()
-        return render(req,"appterceraentrega/padre.html")
-
-    return render(req,"appterceraentrega/cursos.html")
 
 def alumnos(req):
     if req.method == 'POST':
@@ -37,3 +21,31 @@ def alumnos(req):
         formulaio = AlumnoFormulario()
 
     return render(req, "appterceraentrega/alumnos.html",{"formulario": formulaio})
+
+def profesores(req):    
+    if req.method == 'POST':
+        formulaio = ProfesorFormulario(req.POST)
+
+        if formulaio.is_valid():
+            informacion = formulaio.cleaned_data
+            profesor = Profesor(nombre=informacion['nombre'], apellido=informacion['apellido'], profesion=informacion['profesion'])
+            profesor.save()
+            return render(req,"appterceraentrega/padre.html")
+    else:
+        formulaio = ProfesorFormulario()
+
+    return render(req, "appterceraentrega/profesores.html",{"formulario": formulaio})
+
+def cursos(req):
+    if req.method == 'POST':
+        formulaio = CursoFormulario(req.POST)
+
+        if formulaio.is_valid():
+            informacion = formulaio.cleaned_data
+            curso = Curso(nombre=informacion['nombre'], camada=informacion['camada'])
+            curso.save()
+            return render(req,"appterceraentrega/padre.html")
+    else:
+        formulaio = CursoFormulario()
+
+    return render(req, "appterceraentrega/cursos.html",{"formulario": formulaio})
